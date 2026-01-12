5b56004b46879249641cb6824193e526ca8d2ecb
strenxfit-lab <strenxfit@gmail.com>
2026-01-12 18:57:25 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,8 +1,8 @@
 // alert.js
 import axios from "axios";
 
-const TELEGRAM_BOT_TOKEN = "8499533147:AAEU0mkA7l0plfMHoK9zuYYmRdr_YOE-hik"; // Step 1 ka token
-const TELEGRAM_CHAT_ID = "6077248442";     // Step 2 ka ID
+const TELEGRAM_BOT_TOKEN = "8445228246:AAEjlvBb_umDbIEoroqswE3mCYZYh_UQMsc"; // Step 1 ka token
+const TELEGRAM_CHAT_ID = "5132081198";     // Step 2 ka ID
 
 export async function sendAlert(message) {
   const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
